2008e4522a5eb6b98fe23198db94d62cf9f5ee7f
Ponta <ponta@ponpon.com>
2020-12-01 03:49:21 +0900

    ImageLayout.Utilityの追加

---
+++ ImageLayout.Utility/KeyItem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageLayout.Utility
+{
+    [Serializable]
+    /// <summary>
+    /// キャプチャする項目
+    /// </summary>
+    public class KeyItem
+    {
+        /// <summary>
+        /// キー
+        /// </summary>
+        public Keys Key { get; set; }
+
+        /// <summary>
+        /// X座標
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Y座標
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// 幅
+        /// </summary>
+        public int Width { get; set; }
+
+        /// <summary>
+        /// 高さ
+        /// </summary>
+        public int Height { get; set; }
+    }
+}
---
+++ ImageLayout.Utility/SkinInfomation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ImageLayout.Utility
+{
+    [Serializable]
+    public class SkinInfomation
+    {
+        /// <summary>
+        /// スキン名
+        /// </summary>
+        public string SkinName { get; set; }
+
+        /// <summary>
+        /// 押す前の画像
+        /// </summary>
+        public string NormalImage { get; set; }
+
+        /// <summary>
+        /// 押した後の画像
+        /// </summary>
+        public string PressImage { get; set; }
+
+        /// <summary>
+        /// キー情報
+        /// </summary>
+        public KeyItem[] Keys { get; set; }
+    }
+}
